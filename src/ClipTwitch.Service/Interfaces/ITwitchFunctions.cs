95e551c5af2348fef825422546ee0fb2565f1c45
André Macena <andremacena97@gmail.com>
2025-09-09 22:24:57 -0300

    Update ITwitchFunctions.cs

--- src/ClipTwitch.Service/Interfaces/ITwitchFunctions.cs
+++ src/ClipTwitch.Service/Interfaces/ITwitchFunctions.cs
@@ -1,14 +1,10 @@
 ﻿using ClipTwitch.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClipTwitch.Service.Interfaces
 {
     public interface ITwitchFunctions
     {
         public Task<BaseListItems<Streamer>> GetStreamers(string streamerNickname);
+        public Task<BaseListItems<Game>> GetGames(string name);
     }
 }
